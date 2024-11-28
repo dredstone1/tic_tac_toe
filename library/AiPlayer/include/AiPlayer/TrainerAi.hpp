40decdcff9138091e03e39bfb93b83c260103041
maayan <mayanpotu500@gmail.com>
2024-11-28 19:52:48 +0200

    adding TrainerAi files.

---
+++ library/AiPlayer/include/AiPlayer/TrainerAi.hpp
@@ -0,0 +1,11 @@
+#ifndef TRAINERAI_HPP
+#define TRAINERAI_HPP
+
+class TrainerAi {
+public:
+  TrainerAi();
+  int train();
+  ~TrainerAi() = default;
+};
+
+#endif // TRAINERAI_HPP
---
+++ library/AiPlayer/src/TrainerAi.cpp
@@ -0,0 +1,8 @@
+#include "../include/AiPlayer/TrainerAi.hpp"
+#include <iostream>
+
+int TrainerAi::train() {
+  std::cout << "Training AI" << std::endl;
+
+  return 0;
+}
