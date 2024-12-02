b4612461386aecd6160a8a2d4e7a90b341934658
maayan <mayanpotu500@gmail.com>
2024-12-02 21:08:46 +0200

    small change

--- main.cpp
+++ main.cpp
@@ -2,23 +2,26 @@
 #include <AiPlayer/AiPlayer.hpp>
 #include <StandardPlayer/StandardPlayer.hpp>
 #include <TicTacToe/game.hpp>
+#include <iostream>
 
 using namespace TicTacToe;
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    // StandardPlayer StandardPlayer1;
-    // StandardPlayer StandardPlayer2;
-    // cout << "Start Game" << endl;
-    //
-    // Game game(&StandardPlayer1, &StandardPlayer2);
-    //
-    // game.run();
+    StandardPlayer StandardPlayer1;
+    cout << "Start Game" << endl;
 
-    StandardPlayer player;
+    if (argc > 1 && string(argv[1]) == "2") {
+        cout << "2 Players Game" << endl;
+        StandardPlayer StandardPlayer2;
+        Game game(&StandardPlayer1, &StandardPlayer2);
+        game.run();
+        return 0;
+    }
+
+    std::cout << "AI Game" << std::endl;
     AiPlayer aiPlayer;
-    Game game(&player, &aiPlayer);
-
+    Game game(&StandardPlayer1, &aiPlayer);
     game.run();
 
     return 0;
